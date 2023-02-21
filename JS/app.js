e0130ef602e871ba0985c33125654f8ba526e1d3
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2023-02-21 13:02:36 +0300

    Fixed Scroll Bug !

--- JS/app.js
+++ JS/app.js
@@ -3,11 +3,13 @@ const item = document.querySelectorAll(".item");
 const card = document.querySelectorAll(".card");
 const backGround = document.querySelector("#bg");
 const closeBtn = document.querySelectorAll(".fa-xmark");
+const body = document.getElementById("body");
 
 for (let i = 0; i < item.length; i++) {
   item[i].addEventListener("click", function () {
     card[i].classList.remove("hidden");
     backGround.classList.add("deneme");
+    body.style.overflowY = "hidden";
   });
 }
 
@@ -17,6 +19,7 @@ for (let i = 0; i < closeBtn.length; i++) {
       card[j].classList.add("hidden");
     }
     backGround.classList.remove("deneme");
+    body.style.overflowY = "scroll";
   });
 }
 
@@ -25,6 +28,7 @@ backGround.addEventListener("click", function () {
     card[i].classList.add("hidden");
   }
   backGround.classList.remove("deneme");
+  body.style.overflowY = "scroll";
 });
 
 document.addEventListener("keydown", function (e) {
@@ -33,6 +37,7 @@ document.addEventListener("keydown", function (e) {
       card[i].classList.add("hidden");
     }
     backGround.classList.remove("deneme");
+    body.style.overflowY = "scroll";
   }
 });
 // for (let i = 0; i < card.length; i++) {
